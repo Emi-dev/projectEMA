adb95a941eb7b0cdb510c16e7dd98cc2c40fbdf5
Emi Clar <kotaki@gmail.com>
2020-01-30 20:18:49 -0500

    added functionality to display the whole list of events.

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,42 +24,42 @@ $(document).ready(function() {
             url: queryURL,
             method: "GET"
         }).then(function(response) {
-            console.log(response);
             // check if input(s) is(are) valid and event data included in the response.
             if(Object.keys(response)[0] === "_embedded") {
-                var result = response._embedded.events[0];
-                var eventList = JSON.stringify(response._embedded.events);
-                console.log("eventList: ", eventList);
-
-                // musician's name
-                var musicianName = $("<h2>").addClass("musicianName").text(result.name);
-
-                // event URL
-                var eventURL = $("<a>").attr("href", result.url).text("Click here for " + result.name + "'s Event Ticket Information");
-
-                // event date
-                var eventDateTime = $("<h3>").text(result.dates.start.localDate + ", " +  result.dates.start.localTime);
-
-                var venue = result._embedded.venues[0];
+                //var event = response._embedded.events[0];
+                var eventList = response._embedded.events;
                 
-                // venue's name
-                var venueName = $("<div>").text(venue.name);
+                eventList.forEach(function(event) {
+                    // musician's name
+                    var musicianName = $("<h2>").addClass("musicianName").text(event.name);
 
-                // venue's city
-                var venueCityCountry = $("<div>").text(venue.city.name + ", " + venue.country.name);
-                
-                // create the division "result" and append all to it
-                var eventInfo = $("<div>").addClass("container");
-                eventInfo.append(musicianName, eventURL, eventDateTime, venueName, venueCityCountry);
+                    // event URL
+                    var eventURL = $("<a>").attr("href", event.url).text("Click here for " + event.name + "'s Event Ticket Information");
 
-                // venue's url
-                if("outlets" in result) { // if outlet property (contains venue's URL) exists
-                    venueURL = $("<a>").attr("href", result.outlets[0].url).text(venue.name + "'s information");
-                    eventInfo.append(venueURL);
-                }
-                
-                // append result to body
-                $("body").append(eventInfo);
+                    // event date
+                    var eventDateTime = $("<h3>").text(event.dates.start.localDate + ", " +  event.dates.start.localTime);
+
+                    var venue = event._embedded.venues[0];
+                    
+                    // venue's name
+                    var venueName = $("<div>").text(venue.name);
+
+                    // venue's city
+                    var venueCityCountry = $("<div>").text(venue.city.name + ", " + venue.country.name);
+                    
+                    // create the division "event" and append all to it
+                    var eventInfo = $("<div>").addClass("container");
+                    eventInfo.append(musicianName, eventURL, eventDateTime, venueName, venueCityCountry);
+
+                    // venue's url
+                    if("outlets" in event) { // if outlet property (contains venue's URL) exists
+                        venueURL = $("<a>").attr("href", event.outlets[0].url).text("Venue information");
+                        eventInfo.append(venueURL);
+                    }
+                    
+                    // append event to body
+                    $("body").append(eventInfo);
+                });
             } else {    // if invalid value(s) entered: responded data doesn't include any event data.
                 $("#errorMsg").text("invalid input");
             }        
