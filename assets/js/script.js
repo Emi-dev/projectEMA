3d15d3d9070c2e135f6f3bb1e7d61c0e6f108bc0
Emi Clar <kotaki@gmail.com>
2020-02-03 13:29:31 -0500

    moved initMap function from index.html to script.js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -122,3 +122,13 @@ $(document).ready(function() {
         $("#errorMsg").text("");
     }
 });
+
+function initMap() {
+    // The location of Uluru
+    var uluru = {lat: -25.344, lng: 131.036};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('mapContent'), {zoom: 4, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+}    
